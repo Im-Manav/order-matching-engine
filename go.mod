98752dedcaa693d25a966ba8342c8ec0c354f677
Manav Gupta <manavgupta457@gmail.com>
2025-11-20 02:24:17 +0530

    OME-10 Write matching algorithm

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Im-Manav/order-matching-engine
 
 go 1.25.3
+
+require (
+	github.com/google/uuid v1.6.0
+	modernc.org/libc v1.67.0
+)
--- internal/engine/matcher.go
+++ internal/engine/matcher.go
@@ -1,1 +1,83 @@
 package engine
+
+import (
+	"errors"
+	"time"
+
+	"github.com/Im-Manav/order-matching-engine/pkg/models"
+	"github.com/google/uuid"
+)
+
+func Match(order *models.Order, ob *OrderBook) ([]*models.Trade, error) {
+	switch order.Side {
+	case "BUY":
+		return matchBuyOrder(order, ob)
+	case "SELL":
+		return matchSellOrder(order, ob)
+	default:
+		return nil, errors.New("invalid order side")
+	}
+}
+
+func matchBuyOrder(order *models.Order, ob *OrderBook) ([]*models.Trade, error) {
+	trades := []*models.Trade{}
+	for order.Quantity > 0 {
+		bestAsk := ob.GetBestAsk()
+		if bestAsk == nil || order.Price < bestAsk.Price {
+			break
+		}
+		tradeQty := min(order.Quantity, bestAsk.Quantity)
+		trade := createTrade(order, bestAsk, tradeQty)
+		trades = append(trades, trade)
+
+		order.Quantity -= tradeQty
+		bestAsk.Quantity -= tradeQty
+
+		if bestAsk.Quantity == 0 {
+			ob.RemoveOrder(bestAsk.ID, "SELL")
+		}
+	}
+	if order.Quantity > 0 {
+		ob.AddOrder(order)
+	}
+	return trades, nil
+}
+
+func matchSellOrder(order *models.Order, ob *OrderBook) ([]*models.Trade, error) {
+	trades := []*models.Trade{}
+	for order.Quantity > 0 {
+		bestBid := ob.GetBestBid()
+		if bestBid == nil || order.Price > bestBid.Price {
+			break
+		}
+		tradeQty := min(order.Quantity, bestBid.Quantity)
+		trade := createTrade(bestBid, order, tradeQty)
+		trades = append(trades, trade)
+
+		order.Quantity -= tradeQty
+		bestBid.Quantity -= tradeQty
+
+		if bestBid.Quantity == 0 {
+			ob.RemoveOrder(bestBid.ID, "BUY")
+		}
+	}
+	if order.Quantity > 0 {
+		ob.AddOrder(order)
+	}
+	return trades, nil
+}
+
+func createTrade(buy *models.Order, sell *models.Order, quantity int64) *models.Trade {
+	return &models.Trade{
+		ID:          generateTradeID(),
+		BuyOrderID:  buy.ID,
+		SellOrderID: sell.ID,
+		Quantity:    quantity,
+		Price:       sell.Price,
+		Timestamp:   time.Now().UnixNano(),
+	}
+}
+
+func generateTradeID() string {
+	return uuid.New().String()
+}
